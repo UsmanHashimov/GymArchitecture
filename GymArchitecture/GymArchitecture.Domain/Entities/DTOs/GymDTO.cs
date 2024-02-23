3ed3aa358c06d3071b163b9b3ca2b8f86d03a21d
UsmanHashimov <hashimovusman01@gmail.com>
2024-02-23 15:52:24 +0500

    done

---
+++ GymArchitecture/GymArchitecture.Domain/Entities/DTOs/GymDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GymArchitecture.Domain.Entities.DTOs
+{
+    public class GymDTO
+    {
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public string? location { get; set; }
+        public int rating { get; set; }
+    }
+}
---
+++ GymArchitecture/GymArchitecture.Infrastructure/Migrations/20240223103943_GymMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GymArchitecture.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GymMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gyms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    location = table.Column<string>(type: "text", nullable: true),
+                    rating = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gyms", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gyms");
+        }
+    }
+}
